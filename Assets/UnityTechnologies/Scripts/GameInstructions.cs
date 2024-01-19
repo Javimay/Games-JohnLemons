c0926947cb70d23097b4ce516be034dbfc51a75f
“Javier <diego.mayorga@zemoga.com>
2024-01-18 19:00:06 -0500

    Fix errors, remove some walls and create new objects

--- Assets/UnityTechnologies/Scripts/GameInstructions.cs
+++ Assets/UnityTechnologies/Scripts/GameInstructions.cs
@@ -6,15 +6,13 @@ public class GameInstructions : MonoBehaviour
     private Dialogues _dialogues = new();
     private UIManager _uiManager;
     private string _dataShowInstructions = "KEY_SHOW_INSTRUCTIONS";
-    public bool deleteData;
 
     private void Start() {
         _uiManager = UIManager.Instance;
         _uiManager.HideButtonInteractionInCanvas();
         if (!PlayerPrefs.HasKey(_dataShowInstructions)) {
-            PlayerPrefs.SetInt(_dataShowInstructions, 1);
-            PlayerPrefs.Save();
             SetInstructionsMessage();
+            PlayerPrefs.SetInt(_dataShowInstructions, 0);
         }
     }
 
@@ -22,11 +20,5 @@ public class GameInstructions : MonoBehaviour
         string message = _dialogues.GetDialogue(Dialogues.Key_GameInstructions, Dialogues.Key_GameInstructions);
         string[] messages = message.Split('-');
         _uiManager.SetMessage(null, messages);
-    }
-
-    private void OnValidate() {
-        if (deleteData) {
-            PlayerPrefs.DeleteKey(_dataShowInstructions);
-        }
     }
 }
